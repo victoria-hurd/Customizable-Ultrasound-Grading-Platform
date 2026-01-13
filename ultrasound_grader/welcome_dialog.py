d0cb91f1838fd733929da7462ff7da9a6c6bb65f
VictoriaHurd <85904750+victoria-hurd@users.noreply.github.com>
2026-01-12 17:00:33 -0700

    Using signals for window control

--- ultrasound_grader/welcome_dialog.py
+++ ultrasound_grader/welcome_dialog.py
@@ -1,10 +1,12 @@
-from PyQt6.QtWidgets import (
-    QDialog, QVBoxLayout, QLabel,
-    QRadioButton, QPushButton
-)
+from PyQt6.QtWidgets import (QDialog, QVBoxLayout, QLabel,
+                             QRadioButton, QPushButton)
+from PyQt6.QtCore import pyqtSignal
 
 # Welcome dialog box to choose between Admin and Grader modes
+# Using signals to launch respective main windows
 class WelcomeDialog(QDialog):
+    launch_admin = pyqtSignal()
+    launch_grader = pyqtSignal()
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Ultrasound Grading Tool")
@@ -27,11 +29,7 @@ class WelcomeDialog(QDialog):
 
     def continue_clicked(self):
         if self.admin_radio.isChecked():
-            from admin.admin_window import AdminMainWindow
-            self.window = AdminMainWindow()
-        else:
-            from grader.grader_window import GraderMainWindow
-            self.window = GraderMainWindow()
+            self.launch_admin.emit()
+        elif self.grader_radio.isChecked():
+            self.launch_grader.emit()
 
-        self.window.show()
-        self.close()
