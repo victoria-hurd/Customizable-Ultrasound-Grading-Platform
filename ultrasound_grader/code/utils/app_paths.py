558b28a6ff6d6475eb0437af53629c3f3dd6dbce
VictoriaHurd <85904750+victoria-hurd@users.noreply.github.com>
2026-01-22 13:43:33 -0700

    adding additional modes to move without overwrite func

--- ultrasound_grader/code/utils/app_paths.py
+++ ultrasound_grader/code/utils/app_paths.py
@@ -18,7 +18,7 @@ def ensure_app_directories():
     get_admin_studies_dir().mkdir(parents=True, exist_ok=True)
     get_grader_studies_dir().mkdir(parents=True, exist_ok=True)
 
-def move_without_overwrite(source_path, destination_dir):
+def move_without_overwrite(source_path, destination_dir, mode='move'):
     source_path = Path(source_path)
     destination_dir = Path(destination_dir)
     destination_dir.mkdir(parents=True, exist_ok=True)  # Create destination directory if it doesn't exist
@@ -27,8 +27,11 @@ def move_without_overwrite(source_path, destination_dir):
     destination_path = destination_dir / source_path.name
     
     if not destination_path.exists():
-        # If the file does not exist, just move it
-        shutil.move(str(source_path), str(destination_path))
+        # If the file does not exist, just move it/copy it, depending on mode
+        if mode == 'copy':
+            shutil.copy(str(source_path), str(new_destination_path))
+        else:
+            shutil.move(str(source_path), str(new_destination_path))
         return destination_path
 
     else:
@@ -42,7 +45,10 @@ def move_without_overwrite(source_path, destination_dir):
             new_destination_path = destination_dir / new_filename
             
             if not new_destination_path.exists():
-                # Found a unique name, move the file
-                shutil.move(str(source_path), str(new_destination_path))
+                # Found a unique name, move/copy it, depending on mode
+                if mode == 'copy':
+                    shutil.copy(str(source_path), str(new_destination_path))
+                else:
+                    shutil.move(str(source_path), str(new_destination_path))
                 return new_destination_path
             counter += 1
