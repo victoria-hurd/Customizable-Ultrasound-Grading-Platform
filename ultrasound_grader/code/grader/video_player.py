cc375a214df16a6f0a0dd810a39261d7b75e900a
VictoriaHurd <85904750+victoria-hurd@users.noreply.github.com>
2026-01-22 17:00:26 -0700

    editing slider

--- ultrasound_grader/code/grader/video_player.py
+++ ultrasound_grader/code/grader/video_player.py
@@ -1,49 +1,41 @@
 from PyQt6.QtMultimedia import QMediaPlayer, QAudioOutput
 from PyQt6.QtMultimediaWidgets import QVideoWidget
-from PyQt6.QtCore import QUrl, Qt
-from PyQt6.QtWidgets import QWidget, QVBoxLayout
-
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QSlider
 import os
 
 class VideoPlayer(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
+
+        # Media Player
         self.media_player = QMediaPlayer()
         self.audio_output = QAudioOutput()
         self.media_player.setAudioOutput(self.audio_output)
 
+        # Video Widget
         self.video_widget = QVideoWidget()
         self.media_player.setVideoOutput(self.video_widget)
 
+        # Layout
         layout = QVBoxLayout()
         layout.addWidget(self.video_widget)
         self.setLayout(layout)
 
-        # Track slider externally
+        # Slider
         self.slider = None
-
-        # Connect media_player signals
-        self.media_player.positionChanged.connect(self.update_slider)
-        self.media_player.durationChanged.connect(self.update_slider_range)
+        self.media_player.positionChanged.connect(self._update_slider)
+        self.media_player.durationChanged.connect(self._update_slider_range)
         self.media_player.mediaStatusChanged.connect(self._handle_media_status)
 
-    def set_slider(self, slider):
-        self.slider = slider
-        self.slider.setValue(0)
-        self.slider.sliderMoved.connect(self._slider_moved)
-
-    def update_slider_range(self, duration):
-        if self.slider:
-            self.slider.setRange(0, duration)
-
-    def load_video(self, path):
+    # ---------------- Video Controls ----------------
+    def load_video(self, path: str):
         if not os.path.exists(path):
             raise FileNotFoundError(f"Video not found: {path}")
+        from PyQt6.QtCore import QUrl
         self.media_player.setSource(QUrl.fromLocalFile(path))
         self.media_player.stop()
 
-    def play(self):
-        # If we're at (or extremely close to) the end, restart
+    def play(self): 
         if self.media_player.position() >= self.media_player.duration() - 5:
             self.media_player.setPosition(0)
         self.media_player.play()
@@ -54,8 +46,17 @@ class VideoPlayer(QWidget):
     def stop(self):
         self.media_player.stop()
 
-    # ---------------- Slot for slider ----------------
-    def update_slider(self, position):
+    # ---------------- Slider ----------------
+    def set_slider(self, slider: QSlider):
+        self.slider = slider
+        self.slider.setValue(0)
+        self.slider.sliderMoved.connect(self._slider_moved)
+
+    def _update_slider_range(self, duration):
+        if self.slider:
+            self.slider.setRange(0, duration)
+
+    def _update_slider(self, position):
         if self.slider and not self.slider.isSliderDown():
             self.slider.setValue(position)
 
@@ -64,9 +65,6 @@ class VideoPlayer(QWidget):
 
     def _handle_media_status(self, status):
         if status == QMediaPlayer.MediaStatus.EndOfMedia:
-            self.media_player.pause()
+            self.pause()
             self.media_player.setPosition(self.media_player.duration())
 
-    # ---------------- Frame grab for annotation ----------------
-    def grab_frame(self):
-        return self.video_widget.grab()
