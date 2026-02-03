166504d07097ce583635ebfce443127f86976c50
VictoriaHurd <85904750+victoria-hurd@users.noreply.github.com>
2026-02-02 17:51:56 -0700

    adding exit button

--- src/code/ui/landing_page.py
+++ src/code/ui/landing_page.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel, QPushButton
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel, QPushButton, QApplication
 from code.admin.admin_landing import AdminLanding
 from code.grader.grader_landing import GraderLanding
 
@@ -15,12 +15,15 @@ class LandingPage(QWidget):
 
         admin_btn = QPushButton("Enter as Admin")
         grader_btn = QPushButton("Enter as Grader")
+        exit_btn = QPushButton("Exit Application")
 
         admin_btn.clicked.connect(self.open_admin)
         grader_btn.clicked.connect(self.open_grader)
+        exit_btn.clicked.connect(QApplication.instance().quit)
 
         layout.addWidget(admin_btn)
         layout.addWidget(grader_btn)
+        layout.addWidget(exit_btn)
 
     def open_admin(self):
         self.admin_window = AdminLanding(parent_window=self)
